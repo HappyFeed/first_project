ceb9c8be6d60b9d89ccc2223bbdf362480b97a95
HappyFeed <alejomam2011@gmail.com>
2021-09-30 17:53:19 -0500

    first

---
+++ src/views/Pets.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container grid-list-md fluid>
+        <v-layout wrap>
+            <v-flex xs12 sm4 md3>
+                <v-card>
+                    <v-img src="https://goo.gl/6CQNDo"> </v-img>
+                    <v-card-title>
+                        <div>
+                            <h1>Looking for a dog</h1>
+                        </div>
+                    </v-card-title>    
+                </v-card>
+            </v-flex>    
+        </v-layout>
+    </v-container>
+</template>
